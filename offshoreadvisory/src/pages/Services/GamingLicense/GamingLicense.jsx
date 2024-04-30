0cdd5c8fef3147649556dada52ad1e2f142bab4d
Anna Ziankevich <anna.ziankevich@gmail.com>
2024-04-30 18:44:37 +0300

    GamingLicense & ResidencePassport page

--- offshoreadvisory/src/pages/Services/GamingLicense/GamingLicense.jsx
+++ offshoreadvisory/src/pages/Services/GamingLicense/GamingLicense.jsx
@@ -1,11 +1,158 @@
 import React from 'react';
+import style from './gaming.module.scss'
+import ReadyToContact from '../../../components/ReadyToContact/ReadyToContact'
+import ServicesStartBlock from '../../../components/ServicesStartBlock/ServicesStartBlock'
+import List from '../../../components/List/List'
+import img from '../../../assets/img/picture-contact.jpeg'
 
 const GamingLicense = () => {
 
+    const advantages = [
+        {
+            title: "Company Incorporation Flexibility",
+            text: "There is no requirement for a company to be incorporated in the jurisdiction. Applicants can incorporate in any jurisdiction that aligns best with their business objectives, offering significant flexibility."
+        },
+        {
+            title: "Unified License",
+            text: "The license covers all types of gambling activities, simplifying and reducing costs for operators with a convenient and efficient unified approach."
+        },
+        {
+            title: "White-Label Licensing",
+            text: "The comprehensive license can be used across various online gaming operations like casinos, poker, betting, lotteries, as well as for renting out games and white-label services. This versatility is beneficial for a wide range of gaming activities."
+        },
+        {
+            title: "Suitable for B2B and B2C",
+            text: "The license is applicable for both B2C (business-to-consumer) and B2B (business-to-business) operations, providing versatility for different business models."
+        },
+        {
+            title: "Game Providers",
+            text: 'Currently, only those providers that work with operators without a license or "those that require a license" on a case-by-case basis are collaborating.'
+        },
+        {
+            title: "Payment Solutions",
+            text: "The situation here is more favorable. Payment providers that can be opened without a license are cooperating, along with a medium portion of payment systems that require a license. Compliance for remaining payment systems is assessed on a case-by-case basis. The license could help in acquiring payments that are unavailable to operators without a license, depending on the target countries."
+        },
+        {
+            title: "Banking Options",
+            text: "Currently, there are banks ready to accept operators with this license (at least which I am aware of). This is a significant plus for the license as it facilitates banking and financial transactions for operators."
+        },
+        {
+            title: "Validator for License Verification",
+            text: "One key feature of the license is that it includes a validator. This tool enables players to verify that an operator holds a valid license, significantly enhancing trust and credibility among users."
+        },
+    ]
+
+    const whyUs = [
+        {
+            title: "Insurance Licenses",
+            text: "For operators who have their own games, a unique game, or a novel concept, obtaining a license might be more challenging, and alternatives are limited. In this scenario, the licensing process could be a viable option as it may offer a more accessible pathway to obtaining a license, especially for unique or innovative gaming propositions."
+        },
+        {
+            title: "Operators Needing to Showcase a License to Investors or Players",
+            text: "It is an ideal choice for operators who need to demonstrate their legitimacy to investors or players. The availability of a Validator tool enables easy verification of the license's validity. Additionally, having a company registered for gambling activities, possibly coupled with a company (in the EU), can enhance trust and credibility. The process is described as relatively quick and straightforward, requiring minimal complex documentation, making it attractive for operators seeking efficient licensing."
+        },
+        {
+            title: "Operators Looking for Rapid Licensing and Launch",
+            text: "For those needing to obtain a license swiftly and start operations, it offers a quick turnaround. This expedited process is beneficial for operators who want to launch their services promptly. Furthermore, it provides an opportunity to establish operations"
+        },
+    ]
+
     return (
         <>
+            <div className={style.gaming}>
+                <div className={style.gaming__start}>
+                    <div className={style.gaming__startContent}>
+                        <h2>
+                            Welcome to Offshore Advisory – Your Gateway to <span>Gaming License Excellence in</span>
+                        </h2>
+                    </div>
+                </div>
+                <div className={style.gaming__info}>
+                    <div className={style.gaming__infoContent}>
+                        <div className={style.gaming__infoCards}>
+                            <div className={style.gaming__infoCard}>
+                                <div className={style.gaming__infoCardTitle}>
+                                    The popularity of a global gambling license surged in response to the 2023 reforms in the 
+                                    gaming licensing system, prompting operators to explore alternative jurisdictions. 
+                                </div>
+                                <div className={style.gaming__infoCardDesc}>
+                                    The strategic leadership of the company issuing this license, led by an experienced professional, 
+                                    played a pivotal role in acquiring the right to issue licenses and effectively promoting the 
+                                    license through strategic connections.
+                                </div>
+                            </div>
+                            <div className={style.gaming__infoCard}>
+                                <div className={style.gaming__infoCardTitle}>
+                                    Presently, the operation of this licensing is managed by skilled professionals with the aim of 
+                                    elevating its status. A Google search for "global gaming license" reveals numerous articles from 
+                                    leading companies in the gambling industry and beyond, indicating a growing recognition, which 
+                                    was relatively unknown before.
+                                </div>
+                                <div className={style.gaming__infoCardDesc}>
+                                    The commitment is to providing a fair, safe, and transparent gaming environment for both players 
+                                    and operators. The licensing process is described as straightforward and efficient, incorporating 
+                                    measures against fraud and money laundering, as well as a formal dispute resolution process.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.gaming__restricted}>
+                    <div className={style.gaming__restrictedContent}>
+                        <h3 className={style.gaming__restrictedTitle}>
+                            Restricted Countries&nbsp;
+                            <span>(The license is not tied to any specific jurisdictions, providing a global gaming license without restrictions).</span>
+                        </h3>
+                        <div className={style.gaming__restrictedInfo}>
+                            <div className={style.gaming__restrictedText}>
+                                This selection underscores a clear understanding of feasible and inadvisable operational locations, 
+                                aligning with gaming regulations or unofficially considered non-recommendable for gaming operations. 
+                                This alignment suggests that the licensing authority is not only aware of the global gaming regulatory 
+                                landscape but is also actively taking steps to align with internationally recognized standards and 
+                                practices.
+                            </div>
+                            <div className={style.gaming__restrictedText}>
+                                This approach, which will likely be further elaborated in the chapters titled "Replacement" and 
+                                "The Future," indicates that the regulatory framework is being developed with an eye on the 
+                                broader international context, taking into account both established and emerging norms in the 
+                                global gaming industry. This strategic positioning could play a significant role in shaping the future 
+                                as a competitive and respected licensing jurisdiction in the gaming sector.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.gaming__picture}></div>
+                <div className={style.gaming__whyUs}>
+                    <div className={style.gaming__whyUsContent}>
+                        <div className={style.gaming__whyUsTitle}>
+                            Who is it<br/><span>Suitable for?</span>
+                        </div>
+                        <div className={style.gaming__whyUsInfo}>
+                            {whyUs.map(item => {
+                                return <div className={style.gaming__whyUsItem} key={item.title}>
+                                         <h3 className={style.gaming__whyUsItemTitle}>{item.title}</h3>
+                                         <div className={style.gaming__whyUsItemDesc}>{item.text}</div>
+                                       </div>
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className={style.gaming__advantages}>
+                    <List
+                        title='Is it a Replacement?'
+                        subtitle='(Advantages and Disadvantages)'
+                        infoArray={advantages}
+                    />
+                </div>
+                <div className={style.gaming__contact}>
+                    <ReadyToContact
+                        title='Ready to embark on a journey of international business excellence?'
+                        subtitle='Contact us to learn more about how Offshore Advisory can be your trusted partner in navigating offshore opportunities.'
+                        img={img}
+                    />
+                </div>
+            </div>
         </>
-
     );
 };
 
--- offshoreadvisory/src/pages/Services/ResidencePassport/ResidencePassport.jsx
+++ offshoreadvisory/src/pages/Services/ResidencePassport/ResidencePassport.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
+import style from './residence.module.scss'
+import ReadyToContact from '../../../components/ReadyToContact/ReadyToContact'
+import ServicesStartBlock from '../../../components/ServicesStartBlock/ServicesStartBlock'
+import List from '../../../components/List/List'
+import img from '../../../assets/img/picture-contact.jpeg'
 
 const ResidencePassport = () => {
 
+    const countries = [
+        {
+            title: "Italy",
+            text: "Immerse yourself in Italy's timeless beauty and cultural richness through its investor-friendly residency programs. From the Elective Residence Visa, perfect for those seeking the Italian lifestyle, to the Investor Visa for Entrepreneurs, designed to attract business investors, Italy offers a variety of pathways to residency. Let us navigate the intricacies of Italian bureaucracy, ensuring a seamless transition to your new Italian home."
+        },
+        {
+            title: "Spain",
+            text: "Bask in the Mediterranean sun and embrace the vibrant culture of Spain with our expert assistance in securing residency. Through Spain's Golden Visa Program, investors can obtain residency by investing in real estate, while the Non-Lucrative Visa caters to retirees and individuals with independent means. Our team simplifies the application process, guiding you towards your Spanish residency dreams effortlessly."
+        },
+        {
+            title: "Cyprus",
+            text: "Experience the allure of Cyprus and its golden shores with our comprehensive residency and citizenship solutions. The Cyprus Investment Program and Permanent Residence Permit offer pathways to residency and citizenship through investment in real estate or businesses. Partner with us to navigate Cyprus' favorable regulatory landscape and unlock the benefits of European residency and citizenship."
+        },
+        {
+            title: "Dubai",
+            text: "Embrace the boundless opportunities of Dubai, a global hub of innovation and prosperity, with our expert guidance in securing residency. Whether you're an investor, entrepreneur, or professional, Dubai's array of residency visas, including the Investor Visa and Retirement Visa, cater to diverse needs. Let us facilitate your journey to residency in Dubai, where luxury meets opportunity."
+        },
+    ]
+
+    const services = [
+        {
+            title: "Residency Programs",
+            text: "We provide guidance on residency programs available in each country, including investor visas, golden visas, and residency by investment schemes, helping clients fulfill eligibility requirements and navigate the application process."
+        },
+        {
+            title: "Property Investment",
+            text: "Our team assists clients in identifying and acquiring residential or commercial properties that align with their preferences, budget, and investment goals. From luxury villas and seaside apartments to commercial real estate, we offer expert advice and support at every stage of the transaction."
+        },
+        {
+            title: "Legal and Financial Assistance",
+            text: "We collaborate with trusted legal and financial professionals in each jurisdiction to ensure compliance with local regulations, handle documentation, and facilitate smooth property transactions."
+        },
+    ]
+
     return (
         <>
+            <div className={style.residence}>
+                <div className={style.residence__start}>
+                    <ServicesStartBlock
+                        title='Our Services:'
+                        subtitle="Residence & passport"
+                        text="Whether you're drawn to the charm of Italy, the allure of Spain, the strategic advantages of Cyprus, or the dynamism of Dubai, we provide tailored guidance to make your aspirations a reality."
+                    />
+                </div>
+                <div className={style.residence__countries}>
+                    <div className={style.residence__countriesContent}>
+                        {countries.map(item => {
+                            return <div className={style.residence__countriesItem} key={item.title}>
+                                     <h1 className={style.residence__countriesItemTitle}>{item.title}</h1>
+                                     <div className={style.residence__countriesItemDesc}>{item.text}</div>
+                                   </div>
+                        })}
+                    </div>
+                </div>
+                <div className={style.residence__picture}></div>
+                <div className={style.residence__services}>
+                    <List 
+                        title='At Offshore Advisory, we offer a comprehensive suite of services to facilitate residency and property acquisition in Italy, Spain, and Cyprus'
+                        infoArray={services}
+                    />
+                </div>
+                <div className={style.residence__contact}>
+                    <ReadyToContact
+                        title='Ready to embark on your path to global residency and citizenship?'
+                        subtitle="Contact Offshore Advisory to schedule a consultation with our seasoned advisors. Whether your destination is Italy, Spain, Cyprus, Dubai, or beyond, let us navigate the complexities for you, opening doors to new horizons and boundless opportunities."
+                        img={img}
+                    />
+                </div>
+            </div>
         </>
-
     );
 };
 
